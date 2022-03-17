15d72b39beda5af6024b645493facd5b9d0d4a9c
qwx <qwx@sciops.net>
2022-03-17 07:07:42 +0000

    dijkstra: shim

--- path/dijkstra.c
+++ path/dijkstra.c
@@ -8,9 +8,31 @@
 #include "dat.h"
 #include "fns.h"
 
-int
-mouseinput(Node *, Mouse)
+Node *start, *goal;
+
+static int
+dijkstra(Node *a, Node *g)
 {
+	USED(a,g);
+	return 0;
+}
+
+static int
+findpath(void)
+{
+	if(start == nil || goal == nil)
+		return -1;
+	return dijkstra(start, goal);
+}
+
+int
+mouseinput(Node *n, Mouse m)
+{
+	switch(m.buttons & 7){
+	case 1: goal = n; findpath(); break;
+	case 2: start = n; findpath(); break;
+	case 4: n->blocked ^= 1; break;
+	}
 	return 0;
 }
 
