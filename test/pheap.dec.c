599f5d31aae2b1f3b09df815e5fc70ccd6cf387a
qwx <qwx@sciops.net>
2022-06-02 22:56:26 +0000

    test/pheap: fix, make faster and more useful

--- test/pheap.dec.c
+++ test/pheap.dec.c
@@ -1,33 +1,45 @@
 #include <u.h>
 #include <libc.h>
+#include <bio.h>
 #include "asif.h"
 debuglevel = Logtrace;
 enum{
 	Nnodes = 10000,
 };
 
-void main(void) {
-	Pairheap *queue, *a, **t, **p;
-	int fd, i;
+void
+main(int argc, char **argv)
+{
 	double v;
+	char *suff, name[128];
+	Pairheap *queue, *a, **t, **p;
+	Biobuf *bf, *ubf;
 
+	ARGBEGIN{
+	}ARGEND
+	suff = *argv == nil ? "" : *argv;
 	t = emalloc(Nnodes * sizeof *t);
-	if((fd = create("res", OWRITE, 0644)) < 0)
-		sysfatal("create: %r");
+	snprint(name, sizeof name, "ph.unsorted%s", suff);
+	if((ubf = Bopen(name, OWRITE)) == nil)
+		sysfatal("Bopen: %r");
+	snprint(name, sizeof name, "ph.sorted%s", suff);
+	if((bf = Bopen(name, OWRITE)) == nil)
+		sysfatal("Bopen: %r");
 	queue = nil;
 	srand(time(nil));
 	for(p=t; p<t+Nnodes; p++){
 		v = frand() * 1e6;
 		*p = pushqueue(v, nil, &queue);
 		if(frand() < 0.5){
-			a = t[nrand(p-t)];
-			decreasekey(a, a->n-frand()*1e2, &queue);
+			a = p == t ? *p : t[nrand(p-t)];
+			decreasekey(a, frand() * 1e2, &queue);
 		}
 	}
 	for(p=t; p<t+Nnodes; p++)
-		fprint(fd, "%.2f\n", (*p)->n);
-	close(fd);
+		Bprint(ubf, "%.2f\n", (*p)->n);
+	Bterm(ubf);
 	while((a = popqueue(&queue)) != nil)
-		print("%.2f\n", a->n);
+		Bprint(bf, "%.2f\n", a->n);
+	Bterm(bf);
 	exits(nil);
 }
